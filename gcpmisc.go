9aff8e40545f40237aaec93359a4b55537f47ca6
Alexander Lagerström <alexander@darwin.local>
2019-06-07 17:45:26 +0100

    Adds function that can retrieve the current project ID
    If you are running an application from inside GCP this function
    will retrieve the project ID for the current project by using
    GCP's internal API

---
+++ gcpmisc.go
@@ -0,0 +1,39 @@
+package gcpmisc
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func getProjectId() (string, error) {
+	// Creates http client object
+	var client http.Client
+
+	// Creates the request
+	req, err := http.NewRequest(
+		"GET",
+		"http://metadata.google.internal/computeMetadata/v1/project/projectid",
+		nil,
+	)
+
+	// Adds this HTTP header otherwise google will give you an error.
+	req.Header.Add("MetadataFlavor", "Google")
+
+	// Makes the HTTP request
+	resp, err := client.Do(req)
+
+	// Checks for errors when doing the http request
+	if err != nil {
+		return "", err
+	}
+
+	// Closes the TCP connection when function returns
+	defer resp.Body.Close()
+
+	// Converts the io.Reader object to string
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	bodyString := string(bodyBytes)
+
+	// Returns the projectID
+	return bodyString, nil
+}
